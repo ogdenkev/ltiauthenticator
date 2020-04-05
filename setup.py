046ce21bbecbbedaf82b5cae72bddd65d3a15862
Kevin Ogden <ogdenkev@gmail.com>
2020-04-05 18:25:40 -0400

    Bump version and author info in setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='jupyterhub-ltiauthenticator',
-    version='0.3',
+    version='0.3.1',
     description='JupyterHub authenticator implementing LTI v1',
-    url='https://github.com/yuvipanda/jupyterhub-ltiauthenticator',
-    author='Yuvi Panda',
-    author_email='yuvipanda@gmail.com',
+    url='https://github.com/ogdenkev/ltiauthenticator',
+    author='Kevin Ogden',
+    author_email='ogdenkev@gmail.com',
     license='3 Clause BSD',
     packages=find_packages(),
     install_requires=[
